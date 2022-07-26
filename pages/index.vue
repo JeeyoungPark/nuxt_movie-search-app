c5a5617bb2e6337fe0820ffe71c0dba78ee6baf5
JeeyoungPark <jeeyoung0321@gmail.com>
2022-07-26 14:32:56 +0900

    feat: add TMDB API - TMDB serves movie database - API docs: https://developers.themoviedb.org/3/movies/get-now-playing

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,40 @@
 <template>
   <div class="home">
+    <!-- Hero -->
     <Hero />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'IndexPage',
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  async fetch() {
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const response = await axios.get(
+        'https://api.themoviedb.org/3/movie/now_playing',
+        {
+          params: {
+            api_key: process.env.TMDB_API,
+            page: 1,
+          },
+        }
+      )
+
+      const data = response.data
+
+      data.results.forEach((movie) => {
+        this.movies.push(movie)
+      })
+    },
+  },
 }
 </script>
