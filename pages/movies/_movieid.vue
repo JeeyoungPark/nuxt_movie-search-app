97e84b17918f1b925bf9d80faa95338a0b0003eb
JeeyoungPark <jeeyoung0321@gmail.com>
2022-07-27 18:28:34 +0900

    feat: add movie detail page

---
+++ pages/movies/_movieid.vue
@@ -0,0 +1,133 @@
+<template>
+  <Loading v-if="$fetchState.pending" />
+  <div v-else class="container single-movie">
+    <NuxtLink class="button" :to="{ name: 'index' }">Back</NuxtLink>
+    <div class="movie-info">
+      <div class="movie-img">
+        <img
+          :src="`https://image.tmdb.org/t/p/w500/${movie.poster_path}`"
+          alt=""
+        />
+      </div>
+      <div class="movie-content">
+        <h1>Title: {{ movie.title }}</h1>
+        <p class="movie-fact tagline">
+          <span>Tagline:</span> "{{ movie.tagline }}"
+        </p>
+        <p class="movie-fact">
+          <span>Released:</span>
+          {{
+            new Date(movie.release_data).toLocaleString('en-us', {
+              month: 'long',
+              day: 'numeric',
+              year: 'numeric',
+            })
+          }}
+        </p>
+        <p class="movie-fact">
+          <span>Duration:</span> {{ movie.runtime }} minutes
+        </p>
+        <p class="movie-fact">
+          <span>Revenue:</span>
+          {{
+            movie.revenue.toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            })
+          }}
+        </p>
+        <p class="movie-fact">Overview:</p>
+        {{ movie.overview }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'SingleMovie',
+  data() {
+    return {
+      movie: null,
+    }
+  },
+  async fetch() {
+    await this.getSingleMovie()
+  },
+  fetchDelay: 2000,
+  methods: {
+    async getSingleMovie() {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/movie/${this.$route.params.movieid}`,
+        {
+          params: {
+            api_key: process.env.TMDB_API,
+            language: 'en-US',
+          },
+        }
+      )
+
+      this.movie = data
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.single-movie {
+  color: #fff;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 32px 16px;
+  .button {
+    align-self: flex-start;
+    margin-bottom: 32px;
+  }
+  .movie-info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 32px;
+    color: #fff;
+    @media (min-width: 800px) {
+      flex-direction: row;
+      align-items: flex-start;
+    }
+    .movie-img {
+      img {
+        max-height: 500px;
+        width: 100%;
+        @media (min-width: 800px) {
+          max-height: 700px;
+          width: initial;
+        }
+      }
+    }
+    .movie-content {
+      h1 {
+        font-size: 56px;
+        font-weight: 400;
+      }
+      .movie-fact {
+        margin-top: 12px;
+        font-size: 20px;
+        line-height: 1.5;
+        span {
+          font-weight: 600;
+          text-decoration: underline;
+        }
+      }
+      .tagline {
+        font-style: italic;
+        span {
+          font-style: normal;
+        }
+      }
+    }
+  }
+}
+</style>
